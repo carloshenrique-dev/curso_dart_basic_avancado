aff32061e79e5b3f8ab6fe8a26ec2ee8d5415204
carloshenrique-dev <c.tarabal25@gmail.com>
2021-04-05 23:35:55 -0300

    first commit

---
+++ bin/curso_dart_basico_avancado.dart
@@ -0,0 +1,5 @@
+import 'package:curso_dart_basico_avancado/curso_dart_basico_avancado.dart' as curso_dart_basico_avancado;
+
+void main(List<String> arguments) {
+  print('Hello world: ${curso_dart_basico_avancado.calculate()}!');
+}
---
+++ test/curso_dart_basico_avancado_test.dart
@@ -0,0 +1,8 @@
+import 'package:curso_dart_basico_avancado/curso_dart_basico_avancado.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
