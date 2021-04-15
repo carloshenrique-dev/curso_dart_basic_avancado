0ece4fe9a65d464b7ee99c6eed1b6c381170c943
carloshenrique-dev <c.tarabal25@gmail.com>
2021-04-14 23:28:26 -0300

    switch e expressao OU

---
+++ lib/basico/6- condicionais/2-switchs/0-switch.dart
@@ -0,0 +1,30 @@
+void main() {
+  int dia = 0;
+
+  switch (dia) {
+    case 0:
+      print('Domingo');
+      break;
+    case 1:
+      print('Segunda');
+      break;
+    case 2:
+      print('Terca');
+      break;
+    case 3:
+      print('Quarta');
+      break;
+    case 4:
+      print('Quinta');
+      break;
+    case 5:
+      print('Sexta');
+      break;
+    case 6:
+      print('Sabado');
+      break;
+    default:
+      print('dia invalido');
+      break;
+  }
+}
---
+++ lib/basico/6- condicionais/3-ExpressaoOU/0-expressao_ou.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  var numerica = int.tryParse('10') ?? 0;
+  print(numerica);
+
+  //as duas interrogacoes equivalem a expressao ou
+}
