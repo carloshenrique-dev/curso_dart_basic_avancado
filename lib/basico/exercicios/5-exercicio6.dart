b55bb7a29e49049ff48c93b1830c3dec3e19b4ef
carloshenrique-dev <c.tarabal25@gmail.com>
2021-04-21 19:19:22 -0300

    exercicios resolvidos

--- lib/basico/exercicios/5-exercicio6.dart
+++ lib/basico/exercicios/5-exercicio6.dart
@@ -1,18 +1,15 @@
 void main() {
-  void main() {
-    List paciente = [
-      'Rodrigo Rahman|35',
-      'Guilheme Rahman|10',
-      'João Rahman|20',
-      'Joaquin Rahman|25',
-      'Luan Rahman|23',
-    ];
+  List paciente = [
+    'Rodrigo Rahman|35',
+    'Guilheme Rahman|10',
+    'João Rahman|17',
+    'Joaquin Rahman|28',
+    'Luan Rahman|23',
+  ];
 
-    //var pacientes = paciente.getRange(0, 3).toList();
-    var teste = paciente.removeWhere((element) => element < 18);
+  paciente.removeRange(1, 3);
 
-    for (int i = 0; i < paciente.length; i++) {
-      //print(paciente[i].toString().replaceAll('|', ' tem ') + ' anos');
-    }
+  for (int i = 0; i < paciente.length; i++) {
+    print(paciente[i].toString().replaceAll('|', ' tem ') + ' anos');
   }
 }
